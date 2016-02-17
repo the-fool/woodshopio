2873f6bc137194e7b9ab27e2eb20d7b3a3c35356
the-fool <sketchbang@gmail.com>
2016-02-17 12:30:00 -0600

    add exclusion to gem api

--- woodshop/gems/models.py
+++ woodshop/gems/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 class Gem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name=('gem'))
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name=('gems'))
     title = models.CharField(max_length=128, unique=True)
     description = models.TextField(blank=True, null=True)
     
--- woodshop/gems/serializers.py
+++ woodshop/gems/serializers.py
@@ -4,7 +4,12 @@ from .models import Gem
 from woodshop.users.serializers import UserSerializer
 
 class GemSerializer(serializers.ModelSerializer):
-    author = UserSerializer
+    author = UserSerializer(required=False)
+    
+    def get_validation_exclusions(self, *args, **kwargs):
+        # Need to exclude `user` since we'll add that later based off the request
+        exclusions = super(GemSerializer, self).get_validation_exclusions(*args, **kwargs)
+        return exclusions + ['author']
     
     class Meta:
         model = Gem
