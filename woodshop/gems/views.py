3008ded3136ffb7082bee7d7a1b66ccc581e4eab
the-fool <sketchbang@gmail.com>
2016-02-16 21:29:42 -0600

    add basic gem viewset

--- woodshop/gems/views.py
+++ woodshop/gems/views.py
@@ -1,5 +1,16 @@
-from rest_framework import generics
-from .models import Gem
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
 
-class GemList(generics.ListAPIView):
-    
+from .models import Gem
+from .serializers import GemSerializer
+
+class GemList(mixins.RetrieveModelMixin,
+              mixins.UpdateModelMixin,
+              mixins.ListModelMixin,
+              viewsets.GenericViewSet):
+    """
+    Updates, lists and retrieves Gem objects
+    """
+    queryset = Gem.objects.all()
+    serializer_class = GemSerializer
+    permission_classes = (AllowAny,)
