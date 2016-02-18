39512e7b4728cc7b258e46c8325cdd8cec1fb2c7
the-fool <sketchbang@gmail.com>
2016-02-18 09:39:08 -0600

    routerize the angular

--- woodshop/frontend/bazaar/static/js/bazaar.js
+++ woodshop/frontend/bazaar/static/js/bazaar.js
@@ -2,9 +2,22 @@
     'use strict';
     
     var bazaarApp = ng.module('bazaarApp', [
+    	'ngRoute',
         'bazaar.api',
         'bazaar.directives'
     ]);
     
+    bazaarApp.config(['$routeProvider', function($rp) {
+    	$rp
+    	.when('/Gem/:gemId', {
+    		templateUrl:'static/tpl/detail.html',
+    	})
+    	.when('/', {
+    		templateUrl: 'static/tpl/bazaar.html'
+    	})
+    	.otherwise({
+    		redirectTo: '/'
+    	});
+    }]);
         
 })(angular);
