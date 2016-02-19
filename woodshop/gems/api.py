0f844a6c86ae86d37279b63973b9b90edb338df2
the-fool <sketchbang@gmail.com>
2016-02-18 21:49:59 -0600

    picture api is 1/3 finished

--- woodshop/gems/api.py
+++ woodshop/gems/api.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, generics
 from rest_framework.permissions import AllowAny
 
-from .models import Gem
-from .serializers import GemSerializer
+from .models import Gem, Picture
+from .serializers import GemSerializer, PictureSerializer
 
 class GemViewSet(mixins.RetrieveModelMixin,
               mixins.UpdateModelMixin,
@@ -14,3 +14,19 @@ class GemViewSet(mixins.RetrieveModelMixin,
     queryset = Gem.objects.all()
     serializer_class = GemSerializer
     permission_classes = (AllowAny,)
+
+
+class PictureDetail(generics.RetrieveUpdateDestroyAPIView):
+    model = Picture
+    serializer_class = PictureSerializer
+    permission_classes = [
+        AllowAny
+    ]
+
+class GemPictureList(generics.ListAPIView):
+	model = Picture
+	serializer_class = PictureSerializer
+
+	def get_queryset(self):
+		queryset = super(GemPictureList, self).get_queryset()
+		return queryset.filter(gem__pk=self.kwargs.get('pk'))
--- woodshop/gems/serializers.py
+++ woodshop/gems/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 
-from .models import Gem
+from .models import Gem, Picture
 from woodshop.users.serializers import UserSerializer
 
 class GemSerializer(serializers.ModelSerializer):
     author = UserSerializer(required=False)
+    pictures = serializers.HyperlinkedIdentityField(view_name='gempicture-list')
     
     def get_validation_exclusions(self, *args, **kwargs):
+    	# I don't know what this is for -- it's copied from a tutorial
         # Need to exclude `user` since we'll add that later based off the request
         exclusions = super(GemSerializer, self).get_validation_exclusions(*args, **kwargs)
         return exclusions + ['author']
     
     class Meta:
         model = Gem
+
+
+class PictureSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Picture
+		gem = serializers.Field(source='gem_asset.id')
