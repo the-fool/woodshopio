c3a41a7688183e737bd79cc8d233605112d56edb
Ariel Godinez <godiarie@siu.edu>
2016-02-21 11:22:06 -0600

    refactor userViewSet & start Vendor

--- woodshop/api/users/admin.py
+++ woodshop/api/users/admin.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, unicode_literals
-
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
@@ -13,10 +12,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
 
-
 class CustomUserCreationForm(UserCreationForm):
 
-    # http://james.lin.net.nz/2013/06/08/django-custom-user-model-in-admin-relation-auth_user-does-not-exist/
     def clean_username(self):
         # Since User.username is unique, this check is redundant,
         # but it sets a nicer error message than the ORM. See #13147.
--- woodshop/api/users/api.py
+++ woodshop/api/users/api.py
@@ -1,23 +1,20 @@
+
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
 
 from .models import User
 from .permissions import IsOwnerOrReadOnly
 from .serializers import CreateUserSerializer, UserSerializer
 
-
-class UserViewSet(mixins.CreateModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.ListModelMixin,
-                  viewsets.GenericViewSet):
-    """
-    Creates, Updates, and retrieves User accounts
-    """
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    #add CRUD functionality to Users API
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsOwnerOrReadOnly,)
 
+
+# from tutorial. method based view for creating custom users 
     def create(self, request, *args, **kwargs):
         self.serializer_class = CreateUserSerializer
         self.permission_classes = (AllowAny,)
