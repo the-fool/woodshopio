61e778669125bb6cf114faf51b31cf8363a4c86f
the-fool <sketchbang@gmail.com>
2016-02-22 21:50:53 -0600

    expand gem api tests

--- woodshop/api/gems/test/test_api.py
+++ woodshop/api/gems/test/test_api.py
@@ -10,15 +10,34 @@ from .factories import GemFactory
 class TestGemAPI(APITestCase):
     
     def setUp(self):
-        self.url = reverse('gem-list')
         Gem.objects.all().delete()
         self.gem_data = model_to_dict(GemFactory.create())
         self.client = APIClient()
         
-    def test_get_request_succeeds(self):
-        response = self.client.get(self.url)
+    def test_get_request_succeeds_with_pk_param(self):
+        url = reverse('gem-list')
+        response = self.client.get(url)
         pk = response.data.get('results')[0].get('id')
         eq_(response.status_code, 200)
         gem = Gem.objects.get(pk=pk)
         eq_(gem.title, self.gem_data.get('title'))
-        
+
+    def test_get_request_returns_list_without_pk(self):
+        url = reverse('gem-list')
+        response = self.client.get(url)
+        eq_(response.data.get('count'), 1)
+        GemFactory.create()
+        response = self.client.get(url)
+        eq_(response.data.get('count'), 2)
+
+    def test_get_gem_detail_succeeds(self):
+        gem = GemFactory.create()
+        url = reverse('gem-detail', kwargs={'pk': gem.id})
+        response = self.client.get(url)
+        eq_(response.status_code, 200)
+        eq_(response.data.get('id'), str(gem.id))
+
+    def test_get_gem_detail_fails(self):
+        url = reverse('gem-detail', kwargs={'pk': 'FOO'})
+        response = self.client.get(url)
+        eq_(response.status_code, 404)
