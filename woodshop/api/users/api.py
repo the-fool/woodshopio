38dad7eaf66377a2ffd6a33bc8b657e062cab755
Ariel Godinez <godiarie@siu.edu>
2016-02-22 21:05:07 -0600

    fixed POST & PUT
    switched to generic view for more explicit control. I will eventually
    move Create (POST) to a registration endpoint

--- woodshop/api/users/api.py
+++ woodshop/api/users/api.py
@@ -1,28 +1,24 @@
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, CreateUserSerializer
-from rest_framework import generics
+from rest_framework import generics,viewsets, mixins
 from .permissions import IsOwnerOrReadOnly
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
 
 User = get_user_model()
 
-class UserList(generics.ListAPIView,
-               mixins.CreateModelMixin,
-               mixins.UpdateModelMixin,):
-
+class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsOwnerOrReadOnly,)
 
-    # from tutorial. create override method
     def create(self, request, *args, **kwargs):
         self.serializer_class = CreateUserSerializer
         self.permission_classes = (AllowAny,)
         return super(UserList, self).create(request, *args, **kwargs)
 
-class UserDetail(generics.RetrieveAPIView):
+class UserDetail(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly,)  
+  
