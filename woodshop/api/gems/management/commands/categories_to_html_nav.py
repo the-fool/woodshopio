bb50e830395083dbfc06a2f9090c8862aeb11309
the-fool <sketchbang@gmail.com>
2016-02-24 14:49:18 -0600

    incorporate category param to sidebar controller function

--- woodshop/api/gems/management/commands/categories_to_html_nav.py
+++ woodshop/api/gems/management/commands/categories_to_html_nav.py
@@ -10,23 +10,26 @@ class Command(BaseCommand):
         l = []
         level = ['second','third','fourth','fifth','sixth']
         index = 0
-        def traverse(node):
-        	nonlocal index
-        	for i in node:
-        		if i[1]: # if not leaf
-        			l.append(
-        			   	'<li><a><span ng-click="cats.setCategory()" class="cat-text">{}</span><span class="fa arrow"></span></a>'.format(i[0]) +	
-        			   	'<ul class="nav nav-{}-level collapse" aria-expanded="false" style="height: 0px;">'.format(level[index])
-        			   	)
-        			index += 1
-        			traverse(i[1])
-        		else:   # if leaf
-        			l.append(
-        				'<li><a href="#">{}</a></li>'.format(i[0]))
-        	l.append('</ul></li>')
-        	index -= 1
+        def traverse(name, node):
+            nonlocal index
+            
+            for i in node:
+                name = name + '_' + i[0] if name else i[0]
+                if i[1]: # if not leaf
+                    l.append(
+                       	'<li><a><span ng-click="cats.setCategory(\'{0}\')" class="cat-text">{1}</span><span class="fa arrow"></span></a>'.format(name, i[0]) +	
+                       	'<ul class="nav nav-{}-level collapse" aria-expanded="false" style="height: 0px;">'.format(level[index])
+                       	)
+                    index += 1
+                    traverse(name,i[1])
+                else:   # if leaf
+                	l.append(
+                		'<li><a href="#">{}</a></li>'.format(i[0]))
+                name = '_'.join(name.split('_')[:-1])
+            l.append('</ul></li>')
+            index -= 1
         
-        traverse(Category.CATS)
+        traverse('', Category.CATS)
         with open(os.path.join(os.path.abspath(settings.PROJECT_ROOT), 'woodshop/frontend/bazaar/templates/cat_sidebar.html'), 'w') as out:
         	out.write(''.join(l))
 
