a78772136045479f8c1c2e54a5a7a0eac9e3a0ac
the-fool <sketchbang@gmail.com>
2016-02-24 18:15:12 -0600

    remove unnecessary angular relics

--- woodshop/frontend/static/js/commonServices.js
+++ woodshop/frontend/static/js/commonServices.js
@@ -38,13 +38,6 @@
     }
     ]);
 
-    app.factory('Categories', [
-        '$resource', 
-        function($r) {
-            return $r('/static/categories.json');
-        }
-        ]);
-
     app.factory('DetailGemCache', function() {
         var cache = {};
         var detailGem = {};
