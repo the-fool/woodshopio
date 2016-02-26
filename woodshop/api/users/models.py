9d4004ff1a4999742be842d37832c468c3091ee3
the-fool <sketchbang@gmail.com>
2016-02-26 14:29:59 -0600

    add vendor flag to user model

--- woodshop/api/users/models.py
+++ woodshop/api/users/models.py
@@ -10,6 +10,6 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
-
+    is_vendor = models.BooleanField(default=False)
     def __str__(self):
         return self.username
--- woodshop/frontend/cubicle/views.py
+++ woodshop/frontend/cubicle/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.template.context_processors import request
 
 def dashboard(request):
-    return render(request, 'cubicle/dashboard.html')
+	if not request.user.is_vendor:
+		return render(request, '/')
+	return render(request, 'cubicle/dashboard.html')
