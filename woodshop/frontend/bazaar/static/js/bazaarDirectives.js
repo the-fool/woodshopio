b331d5b05dd15e14e56baf056f953ae6f6fb22c2
the-fool <sketchbang@gmail.com>
2016-02-27 11:34:48 -0600

    fix minor angular src bug

--- woodshop/frontend/bazaar/static/js/bazaarDirectives.js
+++ woodshop/frontend/bazaar/static/js/bazaarDirectives.js
@@ -1,7 +1,7 @@
 (function (ng) {
         'use strict';
 
-        var app = ng.module('bazaar.directives', ['common.services', 'reviews']);
+        var app = ng.module('bazaar.directives', ['bazaar.services', 'reviews']);
         var partialUrl = '/static/partials/bazaar_partials/';
 
         app.directive('categoryList', ['Gem', function(Gem) {
