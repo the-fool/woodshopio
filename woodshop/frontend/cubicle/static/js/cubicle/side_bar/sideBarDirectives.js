5c944459afcdb5f80e69e55600daa692a1102d16
the-fool <sketchbang@gmail.com>
2016-02-27 20:51:51 -0600

    display vendor's gems in side menu

--- woodshop/frontend/cubicle/static/js/cubicle/side_bar/sideBarDirectives.js
+++ woodshop/frontend/cubicle/static/js/cubicle/side_bar/sideBarDirectives.js
@@ -1,26 +1,31 @@
 (function(ng) {
-	var app = ng.module('cubicle.sidebar', ['api']);
+	var app = ng.module('cubicle.sidebar', ['api', 'django.auth']);
 
 	var partialUrl = '/static/js/cubicle/partials/';
 	
-	app.directive('cubicleSidebarGemList', ['Gem', function(Gem) {
+	app.directive('cubicleSidebarGemList', ['Gem', 'djangoAuth', function(Gem, djangoAuth) {
 		
 		function ctrl() {
-			var uid = window.location.search.split('=')[1]; // BRITTLE!
 			this.gems = [];
-			Gem.query({vendor:uid}).$promise.then(function(data) {
-				this.gems = data.results;
-			});
 		};
 		return {
 			restrict: 'E',
 			templateUrl: partialUrl + 'sidebar_gem_list.html',
-			scope: {
-				user: '='
-			},
+			scope:  true,
 			bindToController: true,
 			controller: ctrl,
-			controllerAs: 'vm'
+			controllerAs: 'vm',
+			replace: true,
+			link: function(scope, element, attrs) {
+				scope.$watch(attrs.user, function(value) {
+					if (value) {
+						Gem.query({vendor:value.id}).$promise.then(function(data) {
+							scope.vm.gems = data.results;
+							$('#side-menu').metisMenu(); // need to apply event bindings
+						});
+					}
+				});
+			}
 		};
 
 	}]);
