209f3d960543e6897f0c952dc185f43a0fec5062
the-fool <sketchbang@gmail.com>
2016-02-27 19:13:08 -0600

    vendor frontend is runnig bug free

---
+++ woodshop/frontend/cubicle/static/js/cubicleApp.js
@@ -0,0 +1,14 @@
+(function(ng) {
+	'use strict';
+
+	var app = ng.module('cubicleApp', [
+		'ngRoute',
+        'django.auth',
+        'modals',
+        'reviews',
+        'api',
+        'navbar',
+        'globalController',
+        'common.filters',
+		]);
+})(angular);
