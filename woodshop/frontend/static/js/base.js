79633a656b6bd51ae5c54d3e91913c9bd2e51d46
the-fool <sketchbang@gmail.com>
2016-03-07 12:02:19 -0600

    change media breakpoint for collapsing navs

--- woodshop/frontend/static/js/base.js
+++ woodshop/frontend/static/js/base.js
@@ -11,7 +11,7 @@ $(function() {
     $(window).bind("load resize", function() {
         var topOffset = 50;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
+        if (width < 991) {
             $('div.navbar-collapse').addClass('collapse');
             topOffset = 100; // 2-row-menu
         } else {
