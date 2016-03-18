040934e4df159c2b4e557b333be0ee7d9a3ce0eb
the-fool <sketchbang@gmail.com>
2016-03-18 10:43:43 -0500

    add djangoAuth module to frontend

--- woodshop/frontend/static/app/index.run.js
+++ woodshop/frontend/static/app/index.run.js
@@ -7,7 +7,7 @@
         .run(runBlock);
 
     /** @ngInject */
-    function runBlock($rootScope, $timeout, $state)
+    function runBlock($rootScope, $timeout, $state, djangoAuth)
     {
         // Activate loading indicator
         var stateChangeStartEvent = $rootScope.$on('$stateChangeStart', function ()
@@ -33,5 +33,32 @@
             stateChangeStartEvent();
             stateChangeSuccessEvent();
         });
+        // Auth
+       $rootScope.authenticated = false;
+       // Init the auth service
+       djangoAuth.authenticationStatus(false, false).then(function(data) {
+         $rootScope.authenticated = true;
+         djangoAuth.profile().then(function(data) {
+           $rootScope.user = data;
+         });
+       });
+       // Wait and respond to the logout event.
+       $rootScope.$on('djangoAuth.logged_out', function() {
+         $rootScope.authenticated = false;
+         $state.go('app.bazaar-home');
+       });
+       // Wait and respond to the log in event.
+       $rootScope.$on('djangoAuth.logged_in', function() {
+         $rootScope.authenticated = true;
+         djangoAuth.profile().then(function(data) {
+           $rootScope.user = data;
+           $state.go('app.bazaar-home', {}, {reload: true});
+         });
+       });
+       // If the user attempts to access a restricted page, redirect them back to the main page.
+       $rootScope.$on('$routeChangeError', function(ev, current, previous, rejection){
+         console.error("Unable to change routes.  Error: ", rejection)
+         $state.go('app.bazaar-home');
+       });
     }
 })();
