a469f0104ea3b86381a5fe1ddd6aa642298fdd6e
the-fool <sketchbang@gmail.com>
2016-03-18 18:07:26 -0500

    emend detail photo viewer

--- woodshop/frontend/static/app/main/bazaar/detail/detail.controller.js
+++ woodshop/frontend/static/app/main/bazaar/detail/detail.controller.js
@@ -11,9 +11,24 @@
 
         var vm = this;
         vm.gem = GemDetailData;
-        vm.images = GemPhotos.results.map(function(cv) {
-          return cv.image;
-        });
-      }
+        vm.images = processImages();
 
+
+        function processImages() {
+          /** Simplifies the data structure,
+              and sets the main picture as the first  **/
+              
+          var photos = GemPhotos.results;
+          photos.forEach(function(element, index) {
+            if (element.id === GemDetailData.main_picture.id) {
+              var tmp = photos[index];
+              photos[index] = photos[0];
+              photos[0] = tmp;
+            }
+          });
+          return photos.map(function(cv) {
+            return cv.image;
+          });
+        }
+    }
 })();
