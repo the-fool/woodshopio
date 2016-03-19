d3daed874e769624a25581929f93bca351fa9d94
the-fool <sketchbang@gmail.com>
2016-03-19 15:45:37 -0500

    tighten up the filtered view controller

--- woodshop/frontend/static/app/main/bazaar/filtered/filtered.controller.js
+++ woodshop/frontend/static/app/main/bazaar/filtered/filtered.controller.js
@@ -11,10 +11,62 @@
         var initializing = true;
 
         var vm = this;
-        vm.gems = GemData.results;
+
+        // Set up tab nav
         vm.tabs = parseTabLabels();
+
+        // Set current active tab
         vm.activeTab = $stateParams.subCategory ? $stateParams.subCategory : $stateParams.category;
 
+        // Create map by category filters of gem data
+        vm.gemMap = parseGemCategories();
+
+        // Populate specific filter with data on initialization
+        vm.gemMap[vm.activeTab].gems = GemData.results;
+        vm.gemMap[vm.activeTab].initial = false;
+
+        // Place filler data in all others (in order to structure the pages)
+        // Without pre-existing filler data, the UI is wonky
+        $timeout(function() {
+          vm.tabs.forEach(function(category) {
+            if (category !== vm.activeTab) {
+              vm.gemMap[category].gems = GemData.results;
+            }
+          });
+        }, 250);
+
+
+        // on selection of tab, populate the tabbed content with fresh data
+        $scope.$watch('selectedIndex', function(current, old) {
+          if (current !== undefined) {
+            // on initial load, this code fires with undefined current arg
+            // so, pass on first initialization run
+            if (initializing) {
+              $timeout(function() { initializing = false; });
+            } else {
+              vm.activeTab = vm.tabs[current];
+              // only populate with data once
+              if (vm.gemMap[vm.activeTab].initial === true) {
+                  msApi.request('gem-browse@get', {category : $stateParams.category, sub_category : vm.activeTab},
+                      function(response)
+                      {
+                        vm.gemMap[vm.activeTab].gems = response.results;
+                        vm.gemMap[vm.activeTab].initial = false;
+                      },
+                      function (error)
+                      {
+                        console.log(error);
+                      }
+                  );
+                }
+            }
+
+          }
+        });
+
+        /********************************************
+          Get tab names from the categories constant
+        /*******************************************/
         function parseTabLabels() {
           var tabs = [$stateParams.category];
           for (var i in CATEGORIES) {
@@ -27,31 +79,18 @@
           };
           return tabs;
         }
-        console.log(vm.tabs);
 
-        $scope.$watch('selectedIndex', function(current, old) {
-          if (current !== undefined) {
-            // on initial load, this code fires with undefined current arg
-            if (initializing) {
-              $timeout(function() { initializing = false; });
-            } else {
-              vm.activeTab = vm.tabs[current];
-              $timeout(function() {
-                msApi.request('gem-browse@get', {category : $stateParams.category, sub_category : vm.activeTab},
-                    function(response)
-                    {
-                      vm.gems = response.results;
-                    },
-                    function (error)
-                    {
-                      console.log(error);
-                    }
-                );
-              });
-            }
-          }
-        });
-
+        /********************************************
+          Create a data structure where gems are
+          organized by their respective categories
+        /*******************************************/
+        function parseGemCategories() {
+          var gemMap = vm.tabs.reduce(function(map, category) {
+            map[category] = {initial: true, gems: []};
+            return map;
+          }, {});
+          return gemMap;
+        }
       }
 
 })();
