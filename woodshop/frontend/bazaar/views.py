9ce09be7ede34019bdee9449e28de89520925833
the-fool <sketchbang@gmail.com>
2016-03-19 16:25:01 -0500

    delete old code

--- woodshop/frontend/bazaar/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.template.context_processors import request
-
-from woodshop.api.gems.models import Category
-
-def home_page(request):
-    return render(request, 'bazaar_home.html')
--- woodshop/frontend/cubicle/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
-from django.template.context_processors import request
-
-def dashboard(request):
-	if not request.user.is_authenticated() or not request.user.is_vendor:
-		return redirect('/', request=request)
-	return render(request, 'cubicle/cubicle.html')
