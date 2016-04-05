4e7b335a4ebc29f82841c36160bf627955288008
the-fool <sketchbang@gmail.com>
2016-04-04 19:52:50 -0500

    add half-hearted docs to home controller

--- woodshop/frontend/static/app/main/bazaar/home/home.controller.js
+++ woodshop/frontend/static/app/main/bazaar/home/home.controller.js
@@ -9,9 +9,14 @@
     /** @ngInject */
     function BazaarHomeController(BazaarHomeData, BrowseGemData, CATEGORIES, $state)
     {
+
         var vm = this;
-        // Data
-        vm.helloText = BazaarHomeData.data.helloText;
+        /*
+         *  Data
+         *
+         *  @BrowseGemData  : Pseudo-random collection of front-page gems
+         *  @BazaarHomeData : Collection of news and promotion items
+        */
         vm.gems = BrowseGemData.results;
 
         //////////
